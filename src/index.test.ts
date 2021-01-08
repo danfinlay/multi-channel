f626c0f1c5b3b7d0c039133fb341381af83e63fd
Dan Finlay <dan@danfinlay.com>
2021-01-08 13:42:41 -0800

    Just getting set up, for sharing purposes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,28 @@
-import greeter from './index';
+import multiChannel from './index';
 
-describe('Test', () => {
-  it('greets', () => {
+describe('Basic action', () => {
+  it('can allow one controller to invoke an action on another', async () => {
+    const hub = multiChannel.createHub();
+
+    const guestChannel = hub.create({
+      name: 'guest',
+      permittedActions: ['host.greet'],
+    });
     const name = 'Huey';
-    const result = greeter(name);
-    expect(result).toEqual('Hello, Huey!');
+
+    /** The assertion itself.
+    *   Requires the consumer can register its interest before the producer is even online.
+    **/
+    expect(guestChannel.host.greet( name )).resolves.toEqual('Hello, Huey!');
+
+    const host = {
+	greet: (name: string) => `Hello, ${name}!`,	
+    };
+    hub.create({
+      name: 'host',
+      actions: {
+	greet: host.greet,
+      }
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,62 @@
-export default function (name:string): string {
-  return `Hello, ${name}!`;
+export default {
+  createHub,
+} 
+
+function createHub (hubOpts: { name: string } = { name: `Hub ${Math.random()}`}) {
+
+  const hub: Hub = {
+    create: createChannel,
+    actions: {},
+  };
+
+  function createChannel (channelOpts: ChannelOptions = {
+    name: `Channel ${Math.random()}`,
+    actions: {},
+    permittedActions: [],
+  }) {
+
+    hub.actions[channelOpts.name] = channelOpts.actions;
+
+    const channel = {};
+
+    channelOpts.permittedActions.forEach((actionName) => {
+      const parts = actionName.split('.');
+      
+      let invokableObj: ControllerReference = {};
+      addApprovedMethodsToInvokable(parts, invokableObj, hub); 
+
+    })
+
+    return channel;
+  }
+
+  return hub;
 }
+
+function addApprovedMethodsToInvokable (parts: string[], invokable: ControllerReference, hub: Hub): ControllerReference {
+  if (parts.length === 0 || typeof parts[0] === 'undefined') {
+    return invokable;
+  }
+
+  const key: string | undefined = parts.shift();
+  if (typeof key === 'undefined') {
+    return invokable;
+  }
+
+  const child: ControllerReference = {};
+  invokable[key] = child;
+  return addApprovedMethodsToInvokable(parts, child, hub);
+}
+
+interface Hub {
+  create: (channelOpts: ChannelOptions) => Channel;
+  actions: {[controllerName: string]: ControllerReference};
+}
+
+type Channel = {
+  [key:string]: ControllerReference;
+}
+
+type ChannelOptions = { name: string, permittedActions: string[], actions: ControllerReference } 
+
+type ControllerReference = {[key:string]: ControllerReference | Function }
